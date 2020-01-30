2885bfa3007adda7719ec6bb099ba5c46235262d
Baljeet Kumar <baljeet.kumar@razorpay.com>
2020-01-30 15:33:46 +0530

    add deft by looking at a few examples

--- config.el
+++ config.el
@@ -45,7 +45,7 @@
                     ("r" "References")
                     ("d" "Diary")
                     ("p" "Graph Data")
-                    ("t" "Data Tracker"))))
+                    ("n" "Notes" entry (file+headline "~/org/gtd/notes.org" "Inbox") "* %u %?\n%i\n%a" :prepend t))))
 
 (after! org (add-to-list 'org-capture-templates
                          '("gr" "Recurring Task" entry (file "~/org/gtd/recurring.org")
@@ -330,3 +330,55 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+;;
+;;Copied the following from https://github.com/nmartin84/.doom.d/blob/master/config.org
+(defun my-deft/strip-quotes (str)
+  (cond ((string-match "\"\\(.+\\)\"" str) (match-string 1 str))
+        ((string-match "'\\(.+\\)'" str) (match-string 1 str))
+        (t str)))
+
+(defun my-deft/parse-title-from-front-matter-data (str)
+  (if (string-match "^title: \\(.+\\)" str)
+      (let* ((title-text (my-deft/strip-quotes (match-string 1 str)))
+             (is-draft (string-match "^draft: true" str)))
+        (concat (if is-draft "[DRAFT] " "") title-text))))
+
+(defun my-deft/deft-file-relative-directory (filename)
+  (file-name-directory (file-relative-name filename deft-directory)))
+
+(defun my-deft/title-prefix-from-file-name (filename)
+  (let ((reldir (my-deft/deft-file-relative-directory filename)))
+    (if reldir
+        (concat (directory-file-name reldir) " > "))))
+
+(defun my-deft/parse-title-with-directory-prepended (orig &rest args)
+  (let ((str (nth 1 args))
+        (filename (car args)))
+    (concat
+      (my-deft/title-prefix-from-file-name filename)
+      (let ((nondir (file-name-nondirectory filename)))
+        (if (or (string-prefix-p "README" nondir)
+                (string-suffix-p ".txt" filename))
+            nondir
+          (if (string-prefix-p "---\n" str)
+              (my-deft/parse-title-from-front-matter-data
+               (car (split-string (substring str 4) "\n---\n")))
+            (apply orig args)))))))
+
+(provide 'my-deft-title)
+
+(use-package deft
+  :commands (deft deft-open-file deft-new-file-named)
+  :config
+  (setq deft-directory "~/org/"
+        deft-auto-save-interval 0
+        deft-use-filename-as-title nil
+        deft-current-sort-method 'title
+        deft-recursive t
+        deft-extensions '("md" "txt" "org")
+        deft-markdown-mode-title-level 1
+        deft-file-naming-rules '((noslash . "-")
+                                 (nospace . "-")
+                                 (case-fn . downcase))))
+(require 'my-deft-title)
+(advice-add 'deft-parse-title :around #'my-deft/parse-title-with-directory-prepended)
